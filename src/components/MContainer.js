f944783eecbe6b956e72b26814dbefad87806939
rere2133 <rere2133@users.noreply.github.com>
2020-12-23 09:45:26 +0800

    empty LS show message

--- src/components/MContainer.js
+++ src/components/MContainer.js
@@ -38,12 +38,11 @@ function MContainer(props) {
     <>
       <h2 className="mt-5 mb-3">{title}</h2>
       <div className="movie-wrap">
-        <div
-          style={{ padding: " 2rem 0.5rem 1rem", color: "#aaa" }}
-          hidden={`${isShowM ? "hidden" : ""}`}
-        >
-          <h5>目前喜好清單是空的，趕緊加入你喜愛的電影吧！</h5>
-        </div>
+        {!isShowM && (
+          <div style={{ padding: " 2rem 0.5rem 1rem", color: "#aaa" }}>
+            <h5>目前喜好清單是空的，趕緊加入你喜愛的電影吧！</h5>
+          </div>
+        )}
         <Desktop>
           <Carousel itemsToScroll={3} itemsToShow={5} showEmptySlots>
             {(movieData ? movieData : []).map((item) => {
